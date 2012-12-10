b48eb60c58e1d7d554adade3ba9ecfe9a4f74672
azhirov <aleks@aradmin.org.ru>
2012-12-10 15:54:26 +0700

    FEATURE Added symbol size switching in CLI utility

--- tuneLZ.cc
+++ tuneLZ.cc
@@ -4,30 +4,81 @@
 #include "Encoder.h"
 #include "Decoder.h"
 
-const Bits::bit_count bits = 8;
-
 int main(int argc, char* argv[]) {
 	bool encode = true;
+	Bits::bit_count bits = 8;
 
-	if(argc > 1) {
-		std::string arg1(argv[1]);
+	for(int i = 1; i < argc; i++) {
+		std::string arg1(argv[i]);
 
 		if(arg1 == "-e" || arg1 == "e") {
 			encode = true;
 		} else if(arg1 == "-d" || arg1 == "d") {
 			encode = false;
+		} else if(arg1 == "-1" || arg1 == "-2" || arg1 == "-4" || arg1 == "-8") {
+			bits = arg1[1] - '0';
 		} else {
-			std::cerr << "USAGE: " << argv[0] << " [-e|e|-d|d]" << std::endl;
+			std::cerr << "USAGE: " << argv[0] << " [-e|-d] [-1|-2|-4|-8]" << std::endl;
 			return 1;
 		}
 	}
 
 	if(encode) {
-		Encoder<bits> coder;
-		coder.encode(std::cin, std::cout);
+		switch(bits) {
+			case 1:
+				{
+					Encoder<1> coder;
+					coder.encode(std::cin, std::cout);
+				}
+				break;
+			case 2:
+				{
+					Encoder<2> coder;
+					coder.encode(std::cin, std::cout);
+				}
+				break;
+			case 4:
+				{
+					Encoder<4> coder;
+					coder.encode(std::cin, std::cout);
+				}
+				break;
+			case 8:
+			default:
+				{
+					Encoder<8> coder;
+					coder.encode(std::cin, std::cout);
+				}
+				break;
+		}
 	} else {
-		Decoder<bits> coder;
-		coder.decode(std::cin, std::cout);
+		switch(bits) {
+			case 1:
+				{
+					Decoder<1> coder;
+					coder.decode(std::cin, std::cout);
+				}
+				break;
+			case 2:
+				{
+					Decoder<2> coder;
+					coder.decode(std::cin, std::cout);
+				}
+				break;
+			case 4:
+				{
+					Decoder<4> coder;
+					coder.decode(std::cin, std::cout);
+				}
+				break;
+			case 8:
+			default:
+				{
+					Decoder<8> coder;
+					coder.decode(std::cin, std::cout);
+				}
+				break;
+		}
 	}
 
 	return 0;
